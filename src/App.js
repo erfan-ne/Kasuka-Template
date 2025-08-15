61066823a9109d49982fac4471abb46a3365c12c
erfan-ne <erfan.neshati@gmail.com>
2025-08-15 16:33:31 +0330

    style: add base background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import Header from "./Header/Header";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      hello
     </div>
   );
 }
