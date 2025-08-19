87ed40c6bed32765eae8b8bde5b69c6b9d173195
erfan-ne <erfan.neshati@gmail.com>
2025-08-19 20:08:09 +0330

    add about section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import Header from "./Header/Header";
+import About from './About/About';
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <About/>
     </div>
   );
 }
