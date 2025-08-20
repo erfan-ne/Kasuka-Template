1a71e851674937bd89c54d86490deaa05c1cca1f
erfan-ne <erfan.neshati@gmail.com>
2025-08-20 22:59:58 +0330

    update about section

--- src/About/About.js
+++ src/About/About.js
@@ -3,10 +3,14 @@ import "./About.css"
 
 export default function About() {
   return (
-    <div className='about-wrapper'>
+    <section className='about'>
         <div className="about-container">
-
+          <img className='about-image' src="./Images/about-image.jpg" alt="about-image" />
+          <p className="about-title">
+            <h3>Kasuka Digital Company</h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+          </p>
         </div>
-    </div>
+    </section>
   )
 }
